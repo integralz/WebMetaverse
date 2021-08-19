189b5f3c6f6a58f48db635b65a2aeabcb8b08d6f
integralz <luckystay12@gmail.com>
2021-08-19 14:12:42 +0900

    signup form

--- code/app.js
+++ code/app.js
@@ -11,8 +11,12 @@ app.use(express.static(__dirname + '/asset'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/asset/html/login.html");
-})
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + "/asset/html/signup.html");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
