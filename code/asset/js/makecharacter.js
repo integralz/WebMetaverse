7c0071fa0f63aba8484e80924f864bd063b2911e
integralz <luckystay12@gmail.com>
2021-09-07 04:21:42 +0900

    make character's skeleton for animation

--- code/asset/js/makecharacter.js
+++ code/asset/js/makecharacter.js
@@ -14,7 +14,6 @@ const camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1
 
 camera.position.z = 30;
 camera.position.y = 20;
-//camera.position.x = 30;
 camera.lookAt(0, 0, 0);
 //renderer 설정
 const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
@@ -96,6 +95,20 @@ const character_position = [
     0, 10, 0
 ];
 
+//bone 위치 설정
+const bone_position = [
+    -2, -6, 0,
+    2, -6, 0,
+    0, -10, 0,
+    -6, 6, 0,
+    6, 6, 0,
+    0, 6, 0
+];
+
+//bone 생성
+const bones = MakeBones();
+const skeleton = new THREE.Skeleton(bones);
+
 let character = new THREE.Object3D();
 let character_geometry = [];
 
@@ -104,10 +117,12 @@ MakeCharacterGeometry(character_geometry);
 for (let character_geometry_index = 0; character_geometry_index < 6; ++character_geometry_index) {
     const texture = new THREE.TextureLoader().load("../character/example.png");
     const material = new THREE.MeshBasicMaterial({ map: texture });
-    const cube = new THREE.Mesh(character_geometry[character_geometry_index], material);
+    const cube = new THREE.SkinnedMesh(character_geometry[character_geometry_index], material);
     cube.position.x = character_position[character_geometry_index * 3];
     cube.position.y = character_position[character_geometry_index * 3 + 1];
     cube.position.z = character_position[character_geometry_index * 3 + 2];
+    cube.add(skeleton.bones[2]);
+    cube.bind(skeleton);
     character.add(cube);
 }
 
@@ -122,15 +137,15 @@ animate();
 
 //0: left_leg, 1: right_leg, 2: body, 3: left_arm, 4:right_arm, 5: head
 function MakeCharacterGeometry(character) {
-    character_geometry.push(MakeBox(4, 12, 4, 'left_leg'));
-    character_geometry.push(MakeBox(4, 12, 4, 'right_leg'));
-    character_geometry.push(MakeBox(8, 12, 4, 'body'));
-    character_geometry.push(MakeBox(4, 12, 4, 'left_arm'));
-    character_geometry.push(MakeBox(4, 12, 4, 'right_arm'));
-    character_geometry.push(MakeBox(8, 8, 8, 'head'));
+    character_geometry.push(MakeBox(4, 12, 4, 'left_leg', 0));
+    character_geometry.push(MakeBox(4, 12, 4, 'right_leg', 1));
+    character_geometry.push(MakeBox(8, 12, 4, 'body', 2));
+    character_geometry.push(MakeBox(4, 12, 4, 'left_arm', 3));
+    character_geometry.push(MakeBox(4, 12, 4, 'right_arm', 4));
+    character_geometry.push(MakeBox(8, 8, 8, 'head', 5));
 }
 //buffergeometry를 통한 custom mesh 생성
-function MakeBox(nx, ny, nz, character_component) {
+function MakeBox(nx, ny, nz, character_component, skeleton_index) {
     const geometry = new THREE.BufferGeometry();
     const x = nx / 2;
     const y = ny / 2;
@@ -194,10 +209,19 @@ function MakeBox(nx, ny, nz, character_component) {
         uvs.push((uv_map[uv_map_index][i][2][0] + move_x) / 64, (uv_map[uv_map_index][i][2][1] + move_y) / 64);
         uvs.push((uv_map[uv_map_index][i][3][0] + move_x) / 64, (uv_map[uv_map_index][i][3][1] + move_y) / 64);
     }
+    //skeleton에 각 좌표를 할당하기 위한 버퍼 생성
+    const skin_indices = [];
+    const skin_weights = [];
+    for (let i = 0; i < 36; ++i) {
+        skin_indices.push(skeleton_index, 0, 0, 0);
+        skin_weights.push(1, 0, 0, 0);
+    }
 
     geometry.setAttribute('uv', new THREE.BufferAttribute(new Float32Array(uvs), 2));
-    return geometry;
+    geometry.setAttribute('skinIndex', new THREE.Uint16BufferAttribute(skin_indices, 4));
+    geometry.setAttribute('skinWeight', new THREE.Float32BufferAttribute(skin_weights, 4));
 
+    return geometry;
 }
 
 const image_change = document.getElementById("image_upload");
@@ -214,9 +238,9 @@ image_change.addEventListener('change', (event) => {
 });
 
 //업로드된 이미지를 기반으로 캐릭터 재생성
-function MakeCharacterByUploadImg(img){
+function MakeCharacterByUploadImg(img) {
     const texture = new THREE.TextureLoader().load(img);
-    for(let character_index = 0; character_index < 6; ++character_index){
+    for (let character_index = 0; character_index < 6; ++character_index) {
         character.children[character_index].material.map.dispose();
         character.children[character_index].material.map = texture;
     }
@@ -225,7 +249,7 @@ function MakeCharacterByUploadImg(img){
 const make_character = document.getElementById("make_character");
 
 //생성된 캐릭터를 json 형식으로 서버에 post
-make_character.addEventListener('click', ()=>{
+make_character.addEventListener('click', () => {
     const form = document.createElement('form');
     form.setAttribute('method', 'post');
     form.setAttribute('action', '/makecharacter');
@@ -234,7 +258,7 @@ make_character.addEventListener('click', ()=>{
     id.setAttribute('name', 'id');
     id.setAttribute('value', document.cookie.split('id=')[1]);
     form.appendChild(id);
-    
+
     const character_form_input = document.createElement('input');
     character_form_input.setAttribute('name', 'character_json');
     character_form_input.setAttribute('value', JSON.stringify(character.toJSON()));
@@ -242,3 +266,21 @@ make_character.addEventListener('click', ()=>{
     document.body.appendChild(form);
     form.submit();
 });
+
+//bone 생성을 위한 함수
+function MakeBones() {
+    const bones = [];
+    for (let insert_time = 0; insert_time < 6; ++insert_time) {
+        const bone = new THREE.Bone();
+        bone.position.set(bone_position[3 * insert_time], bone_position[3 * insert_time + 1], bone_position[3 * insert_time + 2]);
+        bones.push(bone);
+    }
+    //몸체에 해당하는 것이 bone[2]이므로 이를 루트로 가지게 만든다. 
+    for (let root_add_index = 0; root_add_index < 6; ++root_add_index) {
+        if (root_add_index !== 2) {
+            bones[2].add(bones[root_add_index]);
+        }
+    }
+
+    return bones;
+}
